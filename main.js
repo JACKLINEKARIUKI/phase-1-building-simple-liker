5644e56c69a358451c8c63f52d308e08a9c09465
JACKLINEKARIUKI <jacklinewk71@gmail.com>
2022-08-29 11:12:07 +0300

    Completed assignment

--- main.js
+++ main.js
@@ -2,10 +2,42 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+function simpleLiker(){
+  const error = document.getElementById('modal');
+  error.className = 'hidden';
+  const likeGlyphCollection = document.querySelectorAll('.like-glyph');
+  for (const likeGlyph of likeGlyphCollection) {
+  likeGlyph.addEventListener('click', (event) => {
+    mimicServerCall()
+    .then((result) => {
+      if (result) {
+        checkLikeGlyph(likeGlyph);
+      }
+    })
+    .catch(() =>  {
+      error.className = '';
+      const errorMessage = error.querySelector('h2');
+      errorMessage.innerText = "Random server error. Try again.";
+      setTimeout(() => {
+        error.className = 'hidden';
+      }, 3000);
+    })
+  });
+}
+}
+simpleLiker();
 
 
-
+function checkLikeGlyph(likeGlyph) {
+  if (likeGlyph.innerHTML === EMPTY_HEART) {
+    likeGlyph.innerHTML = FULL_HEART;
+    likeGlyph.className = 'like-glyph activated-heart';
+  }
+  else if (likeGlyph.innerHTML === FULL_HEART) {
+    likeGlyph.innerHTML = EMPTY_HEART;
+    likeGlyph.className = 'like-glyph';
+  }
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
